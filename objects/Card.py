e4f5e65d2ae879543d93afc17ecc33e048ce593f
Alon Salzmann <alonzaltsman@gmail.com>
2018-12-16 15:12:46 -0800

    Initial commit

---
+++ objects/Card.py
@@ -0,0 +1,15 @@
+class Card(object):
+    "Card object containing a number and a symbol"
+    def __init__(self, number, symbol):
+        self.number = number
+        self.symbol = symbol
+
+    def __str__(self):
+        return "symbol: {}, number: {}".format(self.symbol, self.number)
+
+    def __repr__(self):
+        return str(self)
+
+
+card_numbers = range(1,14,1)
+card_symbols = ["spades","hearts","diamonds","clubs"]
---
+++ objects/Deck.py
@@ -0,0 +1,27 @@
+import random
+from .Card import Card, card_numbers, card_symbols
+
+class Deck(object):
+    def __init__(self):
+        self.cards = []
+        for number in card_numbers:
+            for symbol in card_symbols:
+                temp_card = Card(number, symbol)
+                self.cards.append(temp_card)
+
+    def draw(self):
+        return self.cards.pop(0)
+
+    def shuffle(self):
+        temp_cards = {}
+        for card in self.cards:
+            random_number = random.random() #number between 0 and 1
+            temp_cards[random_number] = card
+        temp_numbers = temp_cards.keys() #.keys() return a list of the dict keys
+        temp_numbers.sort()
+        temp_order = []
+        for number in temp_numbers:
+            temp_order.append(temp_cards[number])
+        self.cards = temp_order
+        return self.cards
+
