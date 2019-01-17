74976bdcf011e04720c36e1633ecddd57b8816f4
Alon Salzmann <alonzaltsman@gmail.com>
2019-01-16 18:21:50 -0800

    fixed numbers

--- objects/Card.py
+++ objects/Card.py
@@ -10,5 +10,5 @@ class Card(object):
         return str(self)
 
 
-card_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 11]
+card_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
 card_symbols = ["spades", "hearts", "diamonds", "clubs"]
